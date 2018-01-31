b73d408dbbc42ce90487235170e13e86fff68684
Norah <358682030@qq.com>
2018-01-31 21:45:48 +0800

    加入商品详情页信息

--- src/components/L_sale_main.vue
+++ src/components/L_sale_main.vue
@@ -10,7 +10,183 @@
                <div class="L_sale_main_product_left">
                    <img src="../img/l_img/pic01.jpg">
                </div>
+               <div class="L_sale_main_product_right">
+                   <h3>
+                       <span>品牌</span>
+                       <span>台资味（北京宣武门店）</span>
+                   </h3>
+                   <span>票</span>
+                   <div class="L_xiaoliang">
+                       <img src="../img/l_img/xingxing.jpg" >
+                       <h4>4.6</h4>
+                       <span>月售4678单</span>
+                       <span class="L_ZS">蜂鸟专送</span><br/>
+                       <h4>
+                           <span>￥25元起送</span>|
+                           <span>配送费￥5.18元</span>
+                       </h4>
+                       <h4>
+                           <span>1.62km</span>|
+                           <span>27分钟</span>
+                       </h4>
+                       <br>
+                       <h4>
+                           <img src="../img/l_img/iconP1.jpg">
+                           <span>口碑人气好店</span>
+                       </h4>
+                   </div>
+                   <div class="L_shouxuan">
+                       <h4>
+                           <img src="../img/l_img/iconP2.jpg" alt="">
+                           <span>新用户下单立减17元</span>
+                           <div class="L_ShangJia_huodong">
+                               <span>5个活动</span>
+                               <img src="../img/l_img/iconP4.png" alt="">
+                           </div>
+                       </h4>
+                       <h4>
+                           <img src="../img/l_img/iconP3.jpg" alt="">
+                           <span>新40减20元</span>
+                       </h4>
+                   </div>                   
+               </div>
            </div>
+           <div class="L_sale_main_product">
+               <div class="L_sale_main_product_left">
+                   <img src="../img/l_img/pic01.jpg">
+               </div>
+               <div class="L_sale_main_product_right">
+                   <h3>
+                       <span>品牌</span>
+                       <span>台资味（北京宣武门店）</span>
+                   </h3>
+                   <span>票</span>
+                   <div class="L_xiaoliang">
+                       <img src="../img/l_img/xingxing.jpg" >
+                       <h4>4.6</h4>
+                       <span>月售4678单</span>
+                       <span class="L_ZS">蜂鸟专送</span><br/>
+                       <h4>
+                           <span>￥25元起送</span>|
+                           <span>配送费￥5.18元</span>
+                       </h4>
+                       <h4>
+                           <span>1.62km</span>|
+                           <span>27分钟</span>
+                       </h4>
+                       <br>
+                       <h4>
+                           <img src="../img/l_img/iconP1.jpg">
+                           <span>口碑人气好店</span>
+                       </h4>
+                   </div>
+                   <div class="L_shouxuan">
+                       <h4>
+                           <img src="../img/l_img/iconP2.jpg" alt="">
+                           <span>新用户下单立减17元</span>
+                           <div class="L_ShangJia_huodong">
+                               <span>5个活动</span>
+                               <img src="../img/l_img/iconP4.png" alt="">
+                           </div>
+                       </h4>
+                       <h4>
+                           <img src="../img/l_img/iconP3.jpg" alt="">
+                           <span>新40减20元</span>
+                       </h4>
+                   </div>                   
+               </div>
+           </div>
+           <div class="L_sale_main_product">
+               <div class="L_sale_main_product_left">
+                   <img src="../img/l_img/pic01.jpg">
+               </div>
+               <div class="L_sale_main_product_right">
+                   <h3>
+                       <span>品牌</span>
+                       <span>台资味（北京宣武门店）</span>
+                   </h3>
+                   <span>票</span>
+                   <div class="L_xiaoliang">
+                       <img src="../img/l_img/xingxing.jpg" >
+                       <h4>4.6</h4>
+                       <span>月售4678单</span>
+                       <span class="L_ZS">蜂鸟专送</span><br/>
+                       <h4>
+                           <span>￥25元起送</span>|
+                           <span>配送费￥5.18元</span>
+                       </h4>
+                       <h4>
+                           <span>1.62km</span>|
+                           <span>27分钟</span>
+                       </h4>
+                       <br>
+                       <h4>
+                           <img src="../img/l_img/iconP1.jpg">
+                           <span>口碑人气好店</span>
+                       </h4>
+                   </div>
+                   <div class="L_shouxuan">
+                       <h4>
+                           <img src="../img/l_img/iconP2.jpg" alt="">
+                           <span>新用户下单立减17元</span>
+                           <div class="L_ShangJia_huodong">
+                               <span>5个活动</span>
+                               <img src="../img/l_img/iconP4.png" alt="">
+                           </div>
+                       </h4>
+                       <h4>
+                           <img src="../img/l_img/iconP3.jpg" alt="">
+                           <span>新40减20元</span>
+                       </h4>
+                   </div>                   
+               </div>
+           </div>
+           <div class="L_sale_main_product">
+               <div class="L_sale_main_product_left">
+                   <img src="../img/l_img/pic01.jpg">
+               </div>
+               <div class="L_sale_main_product_right">
+                   <h3>
+                       <span>品牌</span>
+                       <span>台资味（北京宣武门店）</span>
+                   </h3>
+                   <span>票</span>
+                   <div class="L_xiaoliang">
+                       <img src="../img/l_img/xingxing.jpg" >
+                       <h4>4.6</h4>
+                       <span>月售4678单</span>
+                       <span class="L_ZS">蜂鸟专送</span><br/>
+                       <h4>
+                           <span>￥25元起送</span>|
+                           <span>配送费￥5.18元</span>
+                       </h4>
+                       <h4>
+                           <span>1.62km</span>|
+                           <span>27分钟</span>
+                       </h4>
+                       <br>
+                       <h4>
+                           <img src="../img/l_img/iconP1.jpg">
+                           <span>口碑人气好店</span>
+                       </h4>
+                   </div>
+                   <div class="L_shouxuan">
+                       <h4>
+                           <img src="../img/l_img/iconP2.jpg" alt="">
+                           <span>新用户下单立减17元</span>
+                           <div class="L_ShangJia_huodong">
+                               <span>5个活动</span>
+                               <img src="../img/l_img/iconP4.png" alt="">
+                           </div>
+                       </h4>
+                       <h4>
+                           <img src="../img/l_img/iconP3.jpg" alt="">
+                           <span>新40减20元</span>
+                       </h4>
+                   </div>                   
+               </div>
+           </div>
+           
        </div>
     </div>
 </template>
@@ -36,7 +212,7 @@ export default {
         height: 0.31rem;
         width: 100%;
         text-align: center;
-        margin: .73rem auto .48rem auto
+        margin: .73rem auto 0 auto
     }
     .L_sale_main_header span{
         color: #000000;
@@ -46,13 +222,178 @@ export default {
         color: #999999;
        
     }
+    .L_sale_main_product{
+      height: 3rem;
+       width: 100%;
+       border-bottom:1px solid #f2f2f2; 
+       margin: .48rem 0 0 0;
+    }
     .L_sale_main_product_left{
         width: 1.31rem;
         height: 1.31rem;
         margin: 0 .21rem 0 .21rem;
+        float: left;
     }
     .L_sale_main_product_left img{
         width: 100%;
         height: 100%;
     }
+    .L_sale_main_product_right{
+        width: 5.77rem;
+        height: 2rem;       
+        float: right;
+    }
+    .L_sale_main_product_right h3{
+        width: 5.3rem;
+        float: left;
+        margin: 0;
+        line-height: .45rem;
+    }
+    .L_sale_main_product_right h3>span:first-child{
+        display: inline-block;
+        width: .54rem;
+        height: .34rem;
+        line-height: .34rem;
+        border-radius: 0.07rem;
+        background: #ffef09;
+        color: #6f3f15;
+        font-size: .2rem;        
+        text-align: center;
+        float: left;
+        margin: 0.05rem .1rem 0 0;
+       
+    }
+    .L_sale_main_product_right h3 span:last-child{
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        color: #333333;
+        float: left;
+        font-weight: 700;
+        font-size: .4rem;
+        float: left;
+
+    }
+    .L_sale_main_product_right>span{
+        width: .26rem;
+        height: .26rem;
+        display: inline-block;
+        float: right;
+        border:1px solid #dddddd;
+        margin: 0.05rem 0.13rem 0 0;
+        text-align: center;
+        line-height: .26rem;
+    }
+    .L_xiaoliang{
+        /* height: 1.05rem; */
+        width: 5.62rem;
+        border-bottom:1px dashed #d3d3d3; 
+        float: left;
+        margin: .12rem 0 0 0;
+        padding: 0 0 .17rem 0;
+        overflow: hidden;
+    }
+    .L_xiaoliang>img{
+        width: 1.1rem;
+        height: .22rem;
+        float: left;
+        margin: .03rem;
+           
+    }
+    .L_xiaoliang>h4:nth-of-type(1){
+        float: left;
+        font-size: .2rem;
+        margin: 0 .11rem;
+        width: .35rem;
+    }
+    .L_xiaoliang>span:first-child{
+        float: left;  
+        font-size: .2rem;      
+    }
+    .L_xiaoliang .L_ZS{       
+        padding: 0 .02rem;
+        height: .26rem;
+        background-color: #0099ff;
+        border-radius: .04rem;
+        color: white;
+        float: right;
+        text-align: center;
+        line-height: .26rem;
+        font-size: .2rem;
+    }
+    .L_xiaoliang>h4:nth-of-type(2){
+        float: left;
+        width: 3.5rem;
+        color: #796666;
+        line-height: .57rem;
+        margin: 0;
+    }
+    .L_xiaoliang>h4:nth-of-type(2) span:first-child, .L_xiaoliang>h4:nth-of-type(3) span:first-child{
+       padding: 0 .09rem 0 0;
+    }
+    .L_xiaoliang>h4:nth-of-type(3){
+        float: right;
+        width: 2rem;
+        margin: 0;
+        color: #796666;
+        line-height: .5rem;
+    }
+    .L_xiaoliang>h4:nth-of-type(4){
+        line-height: .14rem;
+    }
+    .L_xiaoliang>h4:nth-of-type(4) img{
+        width: .23rem;
+        height: .19rem;
+        float: left;
+    }
+    .L_xiaoliang>h4:nth-of-type(4) span{
+        font-size: .16rem;
+        color: #ec470b;
+        float: left;
+        width: 4rem;
+    }
+    .L_shouxuan{
+        height: 1.11rem;
+        padding: .17rem 0 .27rem 0;
+        float: left;
+
+    }
+    .L_shouxuan h4{
+        float: left;
+        width: 4.6rem;
+        margin: 0;        
+    }
+    .L_shouxuan h4:first-child{
+        width: 100%;
+    }   
+    .L_shouxuan h4>img{
+        width: .28rem;
+        height: .28rem;
+        float: left;
+        margin: 0.07rem 0.12rem 0 0;
+    }
+    .L_shouxuan h4 span{
+        float: left;
+        line-height: .42rem;
+        color: #666666;
+        font-size: .23rem;      
+    }
+    .L_ShangJia_huodong{
+        /* width: .98rem; */
+        margin: 0 .17rem 0 0 ;
+        float: right;
+        /* position: relative; */
+
+    }
+    .L_ShangJia_huodong span{
+        color: #666666;
+        font-size: .23rem; 
+        margin: 0 .1rem 0 0;
+    }
+    .L_ShangJia_huodong img{
+        width: .12rem;
+        height: .09rem;
+        margin: .15rem 0 0 0;
+    }
+
 </style>
