2a95aba90c5d5e7909bd451cdf7ac64f37e127c6
fisher_998 <102145839@qq.com>
2018-01-31 18:14:41 +0800

    新的index.js 和 main.js

--- src/main.js
+++ src/main.js
@@ -3,39 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 // 导入路由模块
-import VueRouter from 'vue-router'
-// 全局安装路由模块
-Vue.use(VueRouter)
-Vue.config.productionTip = false
+import router from './router/index'
 
-// 导入组件
-import Find from './components/find'
-import Mine from './components/mine'
-import Order from './components/order'
-import Sale from './components/sale'
 import './css/reset.css'
 
 
-var router = new VueRouter({
-  // 这里写路由规则
-  routes: [
-    // 这个path就是router-link里的to跳转的地址
-    // component就是根据路由的地址，找到对应的组件
-    { path: '', component: Find },
-    { path: '/find', component: Find },
-    { path: '/mine', component: Mine },
-    { path: '/order', component: Order },
-    { path: '/sale', component: Sale }
-    
-  ]
-})
-/* eslint-disable no-new */
+
+
+
+
 new Vue({
   el: '#app',
   // 需要把路由实例挂载到这个router选项中
-  router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  router,
 })
 
 
