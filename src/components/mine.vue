3a3e006a448794986f3393da2629dedf2ccc5e40
shilecong <1450445494@qq.com>
2018-01-31 19:48:03 +0800

    slc mine_header组件ok  1/31 晚上8:00

--- src/components/mine.vue
+++ src/components/mine.vue
@@ -1,16 +1,23 @@
 <template>
     <div class="mine">
-        <h1>{{ msg }}</h1>
+        <sMineHeader></sMineHeader>
     </div>
 </template>
 
 <script>
+// 导入子组件的header文件
+import sMineHeader from './s_mine_header.vue'
     export default {
         name: "mine",
         data () {
             return {
                 msg: "我的"
             }
+        },
+        components: {
+            // 用的的子组件
+            sMineHeader
+
         }
     }
 </script>
---
+++ src/components/s_mine_header.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="s_mine_header">
+            <div class="s_mine_header_p">我的</div>
+            <div class="s_mine_header_las">
+                <h2>登录/注册</h2>
+                <p>登录后享受更多特权</p>
+            </div>
+        </div>
+        <div class="s_mine_instruc">
+            <p>
+                <img src="../img/s_mine_img/red.png" alt="">
+                红包
+            </p>
+             <p>
+                <img src="../img/s_mine_img/green.png" alt="">
+                金币
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "mine",
+        data () {
+            return {
+                msg: "我的"
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+  @import '../css/s_mine_header.css';
+</style>
