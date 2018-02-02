46c79225a096452011976c70a23a75677d69259a
Norah <358682030@qq.com>
2018-02-02 21:23:21 +0800

    更改index中的错误信息

--- src/router/index.js
+++ src/router/index.js
@@ -21,25 +21,25 @@ import SMineHeaderRedPaperShop from '../components/s_mine_header_redPaper_shop'
 
 import Login from '../components/z_login'
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 65bdb0b497981800027d95d404b905d03174af56
+
+
+
 import LSaleMerchantHeader from '../components/L_sale_merchant_header'
 
 import LSaleMerchantDiancan from '../components/L_sale_merchant_diancan'
 import LMerchantShangjia from '../components/L_merchant_shangjia'
 import spingjia from '../components/s_pingjia'
 
-<<<<<<< HEAD
-=======
 
 
 
 
 
->>>>>>> 65bdb0b497981800027d95d404b905d03174af56
+
+
+
 export default new VueRouter({
   // 配置H5的history模式, 去掉 #/
   mode: 'history',
@@ -58,22 +58,22 @@ export default new VueRouter({
     ]},
     { path: '/order', component: Order },
     { path: '/sale', component: Sale },
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
+
+
     { path: '/login', component: Login , children: [
       { path: 'notelogin', component: Notelogin },
       { path: 'psdlogin', component: Psdlogin },
     ]}
-=======
 
-<<<<<<< HEAD
+
+
     { path: '/L_sale_merchant_header', component: LSaleMerchantHeader},
 
   
 
-=======
->>>>>>> 65bdb0b497981800027d95d404b905d03174af56
+
+
     { path: '/L_sale_merchant_header', component: LSaleMerchantHeader, children: [
       { path: '', redirect: 'diancan' },
       { path: 'diancan', component: LSaleMerchantDiancan },
@@ -82,11 +82,11 @@ export default new VueRouter({
       // {  },
       // {  }
     ]},
-<<<<<<< HEAD
-=======
->>>>>>> d3a2c6d19fe7dfc14e9a9205b0026fbeca7120f4
 
->>>>>>> 65bdb0b497981800027d95d404b905d03174af56
+
+
+
+
     { path: '/login', component: Login }
   ]
 })
